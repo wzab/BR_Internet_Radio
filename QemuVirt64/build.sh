02d785b861bcb77eb7868d569ee5a8617cec5f1e
Wojciech M. Zabolotny <wzab01@gmail.com>
2020-03-15 16:49:07 +0100

    Corrected BR unpacking commands

--- QemuVirt64/build.sh
+++ QemuVirt64/build.sh
@@ -1,5 +1,5 @@
 wget https://buildroot.org/downloads/buildroot-2020.02.tar.bz2
-tar -cjf buildroot-2020.02.tar.bz2
+tar -xjf buildroot-2020.02.tar.bz2
 cp BR_config buildroot-2020.02/.config
 cd buildroot-2020.02
 make
