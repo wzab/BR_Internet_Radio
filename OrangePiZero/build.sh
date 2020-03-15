a5f39c42aeccf784c6f62d7c1e9d6d38507c7aba
Wojciech M. Zabolotny <wzab01@gmail.com>
2020-03-15 17:46:41 +0100

    Added the DTS patch needed to unlock audio in OrangePiZero.

--- OrangePiZero/build.sh
+++ OrangePiZero/build.sh
@@ -1,6 +1,7 @@
 wget https://buildroot.org/downloads/buildroot-2020.02.tar.bz2
 tar -xjf buildroot-2020.02.tar.bz2
 cp BR_config buildroot-2020.02/.config
+cp 0002-orange-pi-audio.patch buildroot-2020.02/linux/
 cd buildroot-2020.02
 make
 
