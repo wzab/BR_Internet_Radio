c75640fdd4c8a8965f41d5159b3be399a5c09ec1
Wojciech M. Zabolotny <wzab01@gmail.com>
2020-03-19 18:55:02 +0100

    Modified the configuration for Raspberry Pi so that the overlay is automatically added to the right directory in the card. The information how to do it was found in https://github.com/pengutronix/genimage/issues/3#issuecomment-221695162

--- RaspberryPi/build.sh
+++ RaspberryPi/build.sh
@@ -2,7 +2,10 @@
 set -e
 wget https://buildroot.org/downloads/buildroot-2020.02.tar.bz2
 tar -xjf buildroot-2020.02.tar.bz2
-#cp BR_config buildroot-2020.02/.config
-#cd buildroot-2020.02
-#make
+cp BR_config buildroot-2020.02/.config
+cd buildroot-2020.02
+for i in ../patches/* ; do
+   patch -p1 < $i
+done 
+make
 
