b8be3e232ed59aa6da822034056a782f37bc5206
zabolotny wojciech <zabolotnyw@p30116.mini.pw.edu.pl>
2020-04-09 22:34:12 +0200

    Added output/host/bin from Admin and Utility to the host, to allow taking "mkimage" from there...

--- QemuVexpressA9/Admin/user_img/boot_admin.sh
+++ QemuVexpressA9/Admin/user_img/boot_admin.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-mkimage -T script -C none -n 'Start script' -d boot_admin.txt boot.scr
+(
+  export PATH=../buildroot-2020.02/output/host/bin:../../Utility/buildroot-2020.02/output/host/bin:$PATH
+  mkimage -T script -C none -n 'Start script' -d boot_admin.txt boot.scr
+)
