1a7c269879c35e33cc9eb54a8ffa4b376d1d2c6e
Wojciech M. Zabolotny <wzab01@gmail.com>
2020-09-26 01:38:42 +0200

    Version for Orange Pi Zero updated to BR 2020.08

--- OrangePiZero/build.sh
+++ OrangePiZero/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-wget https://buildroot.org/downloads/buildroot-2020.02.tar.bz2
-tar -xjf buildroot-2020.02.tar.bz2
-cp BR_config buildroot-2020.02/.config
-cp 0002-orange-pi-audio.patch buildroot-2020.02/linux/
-cd buildroot-2020.02
-make
+wget https://buildroot.org/downloads/buildroot-2020.08.tar.bz2
+tar -xjf buildroot-2020.08.tar.bz2
+cp BR_config buildroot-2020.08/.config
+cp 0002-orange-pi-audio.patch buildroot-2020.08/linux/
+cd buildroot-2020.08
+#make
 
