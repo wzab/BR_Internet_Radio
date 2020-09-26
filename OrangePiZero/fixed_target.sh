c39c23cfd74866c52594a7a01cb310ff369e8800
Wojciech M. Zabolotny <wzab01@gmail.com>
2020-09-26 13:21:46 +0200

    Modified the radio starting script so that it attempts to restart the Python webapplication. Added script that modifies the generated target image. Modified the mpd configuration to allow playing stations with self-signed certificates. The above script was used for that purpose - it adds: input {   plugin "curl"   verify_peer "no" } at the end of the /etc/mpd/conf

---
+++ OrangePiZero/fixed_target.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Switch off SSL verification for Internet radios
+cat > $1/etc/mpd.conf <<MPD_CONF_CORRECTION
+input {
+  plugin "curl"
+  verify_peer "no"
+}
+MPD_CONF_CORRECTION
+
---
+++ common_overlay/usr/bin/radio.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+while true ; do
+  /usr/bin/python3 radio.py
+  sleep 3
+done
+
