f20c015ee867a4dffa6b785d23397cf98a1b6708
Wojciech Zabołotny <wzab@WZabHP.nasz.dom>
2023-04-17 11:24:24 +0200

    Corrected the build script

--- QemuVirt64/build.sh
+++ QemuVirt64/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 wget https://buildroot.org/downloads/buildroot-2023.02.tar.xz
-tar -xJf buildroot-2023.02.tar.bz2
+tar -xJf buildroot-2023.02.tar.xz
 cp BR_config buildroot-2023.02/.config
 cd buildroot-2023.02
 make
